ffcfff224cf1b60e841b443ea540e47f64774747
sebastianosalzillo <salzillosebastiano@gmail.com>
2024-11-05 16:43:12 +0100

    feat: primo esercizio completato

---
+++ js/script.js
@@ -0,0 +1,22 @@
+const parola = prompt ("inserisci una parola e ti diro se e palindroma");
+
+console.log (parola) ;
+
+let parolaMin = parola.toLowerCase();
+
+console.log (parolaMin);
+
+function palindroma (parolaMin) {
+  let parolaRev = parolaMin.split("").reverse().join("");
+  console.log (parolaRev);
+  return parolaMin === parolaRev;
+}
+
+ 
+
+
+if (palindroma(parolaMin)) {
+    console.log("La parola è palindroma.");
+  } else {
+    console.log("La parola non è palindroma.");
+  }
